1e1c9742b80ec10fd9ce7ee39de08750e33ca723
cavanhaman <cavanhaman@hotmail.com>
2018-10-29 21:26:11 -0300

    Implementando classe Produtos e InterfaceDeServicos

--- app/src/main/java/produtos/Produtos.java
+++ app/src/main/java/produtos/Produtos.java
@@ -16,3 +16,10 @@ public class Produtos {
     public void setProduto(String produto) {this.produto = produto;}
     public void setValor(int valor) {this.valor = valor;}
 }
+
+
+/*
+   {"produto":"Voce pediu um donut.","valor":"10"},
+   {"produto":"Voce pediu um sanduiche de sorvete.","valor":"20"},
+   {"produto":"Voce pediu um FroYo.","valor":"30"} 
+*/
